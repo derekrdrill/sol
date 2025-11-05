6006d6fe8edd61d947fce0ddbd1fff11cc83e69f
Derek Drill <derekrdrill@gmail.com>
2025-11-04 20:06:55 -0500

    chore: 🧹 ⚙️ adds eslint and prettier

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Disable ESLint during builds if needed
+    ignoreDuringBuilds: false,
+    // Specify directories to lint
+    dirs: ['src'],
+  },
+};
 
 export default nextConfig;
